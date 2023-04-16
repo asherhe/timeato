ac03b2234ad872db20027e5f5041f4490439e62f
Asher He <github@asherhe.com>
2023-04-16 13:54:32 -0700

    add footer

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from "./components/app";
 
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
-import "./css/index.scss";
+import "./index.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
